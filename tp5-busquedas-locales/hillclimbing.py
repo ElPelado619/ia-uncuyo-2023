45e3dae2667919094d19c3d4dd8def282bf084d2
ElPelado619 <agustinyornet@gmail.com>
2023-09-17 23:01:26 -0300

    tp5 - hillclimb and simannealing testing

---
+++ tp5-busquedas-locales/hillclimbing.py
@@ -0,0 +1,48 @@
+import random
+
+# Función objetivo: Contabiliza la cantidad de pares de reinas amenazadas en el tablero.
+# Parámetros: board - Lista de enteros que representa un tablero de ajedrez.
+# Valor de retorno: Cantidad de pares de reinas amenazadas en el tablero.
+def h(board):
+    n = len(board)
+    # Contador de pares de reinas amenazadas.
+    h = 0
+    # Recorre el tablero.
+    for i in range(n):
+        # Recorre el tablero a partir de la fila siguiente a la fila actual.
+        for j in range(i+1, n):
+            # Si las reinas están en la misma fila o en la misma diagonal, se incrementa el contador.
+            if board[i] == board[j] or abs(board[i] - board[j]) == j - i:
+                h += 1
+    return h
+
+# Algoritmo Hill Climbing para resolver el problema de las N reinas.
+# Parámetros: n - Tamaño del tablero de ajedrez.
+#             max_evaluations - Número máximo de estados a evaluar.
+# Valor de retorno: Tupla con el estado final, el valor de la función H y el número de estados evaluados.
+def hill_climbing(n, max_evaluations):
+    # Genera un tablero aleatorio.
+    current = [random.randint(0, n-1) for i in range(n)]
+    # Evalúa el tablero generado.
+    current_h = h(current)
+    evaluations = 1
+    # Ciclo principal del algoritmo.
+    while evaluations < max_evaluations and current_h > 0:
+        # Genera un nuevo tablero vecino.
+        neighbor = [current[i] for i in range(n)]
+        col = random.randint(0, n-1)
+        row = random.randint(0, n-1)
+        neighbor[col] = row
+        # Evalúa el tablero generado.
+        neighbor_h = h(neighbor)
+        evaluations += 1
+        # Compara el tablero generado con el mejor tablero actual.
+        if neighbor_h < current_h:
+            current = neighbor
+            current_h = neighbor_h
+            # Si encuentra un tablero sin amenazas, termina la ejecución.
+            if current_h == 0:
+                break
+    return current, current_h, evaluations
+
+
---
+++ tp5-busquedas-locales/main.py
@@ -0,0 +1,71 @@
+import hillclimbing as hc
+import simannealing as sa
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+def solver(n,max_states,option):
+    if option == 1:
+        return hc.hill_climbing(n,max_states)
+    elif option == 2:
+        return sa.simulated_annealing(n,max_states,100,0.99)
+
+def avg_and_std(n,name,option):
+    # Arreglo nulo de 30 elementos
+    states = [0 for i in range(30)]
+    times = [0 for i in range(30)]
+    hs = [0 for i in range(30)]
+
+    for i in range(30):
+        # Empieza a contar el tiempo
+        start = time.time()
+
+        # Ejecuta el algoritmo para 1000 reinas y 10000 estados.
+        result = solver(n,10000,option)
+
+        # Termina de contar el tiempo
+        end = time.time()
+
+        # Almacena el tiempo de ejecución.
+        times[i] = end - start
+
+        # Almacena el número de estados evaluados.
+        states[i] = result[2]
+
+        # Almacena el valor de la función H.
+        hs[i] = result[1]
+
+    avg = np.average(states)
+    std = np.std(states)
+
+    return times
+
+for n in [4,8,10,12,15]:
+    print("\n")
+    t1 = avg_and_std(n,"Hill Climbing",1)
+    print("Avg for " + str(n) + " queens: " + str(np.average(t1)) + " s")
+
+    print("\n")
+    t2 = avg_and_std(n,"Simulated Annealing",2)
+    print("Avg for " + str(n) + " queens: " + str(np.average(t2)) + " s")
+
+    # Grafica en un grafico de cajas y bigotes los tiempos de ejecución de los dos algoritmos,
+    # y permite que las cajas tengan diferentes colores.
+    plt.boxplot([t1,t2],labels=["Hill Climbing","Simulated Annealing"],patch_artist=True)
+
+    # Dibuja la grilla del grafico de color gris y transparencia de 0.2
+    plt.grid(color='gray',alpha=0.2)
+
+    plt.title("Execution time for " + str(n) + " queens")
+    plt.ylabel("Time (s)")
+
+    # Muestra los valores maximos y minimos con una precision de 4 decimales
+    plt.text(1.1,np.max(t1),str(np.round(np.max(t1),4)))
+    plt.text(1.1,np.min(t1),str(np.round(np.min(t1),4)))
+    plt.text(2.1,np.max(t2),str(np.round(np.max(t2),4)))
+    plt.text(2.1,np.min(t2),str(np.round(np.min(t2),4)))
+
+    plt.show()
+
+
+
